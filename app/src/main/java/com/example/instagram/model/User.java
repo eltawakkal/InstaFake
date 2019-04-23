8bf8750fccdae6cbb38bddc4424516cc100355e2
Rizieq <rizieqgifari13012003@gmail.com>
2019-04-23 14:10:55 +0700

    tambah constructor dan setter getter

--- app/src/main/java/com/example/instagram/model/User.java
+++ app/src/main/java/com/example/instagram/model/User.java
@@ -4,6 +4,46 @@ public class User {
     private String id;
     private String nama;
     private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public User(String id, String nama, String email, String pass) {
+        this.id = id;
+        this.nama = nama;
+        this.email = email;
+        this.pass = pass;
+    }
+
     private String pass;
 
 
