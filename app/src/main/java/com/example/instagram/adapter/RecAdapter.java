bb0707a9add633f9000566e0892db5102fd62097
Fadhli A <fadhli.tech@gmail.com>
2019-04-23 14:04:26 +0700

    Continue the adapter code

--- app/src/main/java/com/example/instagram/adapter/RecAdapter.java
+++ app/src/main/java/com/example/instagram/adapter/RecAdapter.java
@@ -1,4 +1,30 @@
 package com.example.instagram.adapter;
 
-public class RecAdapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RecAdapter extends RecyclerView.Adapter<RecAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
